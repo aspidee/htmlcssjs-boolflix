2b0e67723a072c0acfb2360bcaeea37ff765b533
aspidee <gabrielemandara@gmail.com>
2019-03-31 00:55:55 +0100

    milestone 2 and 4

--- script.js
+++ script.js
@@ -1,50 +1,47 @@
-function addTitle(title, title_orig, language, vote) {
 
-  var tempData = {
 
-    title: title,
-    title_orig: title_orig,
-    language: language,
-    vote: vote,
-  }
+function clearOldResult() {
+
+  var films = $(".film_box");
+  films.remove();
+
+  var inputSearch = $("#input_search");
+  inputSearch.val("");
+}
+
+function printFilms(dataList) {
+
+  clearOldResult();
+
+  var container = $(".films");
 
   var template = $("#film_template").html();
   var compiled = Handlebars.compile(template);
-  var li = compiled(tempData);
-
-  var ulFilms = $(".films");
-  ulFilms.append(li);
-}
 
 
-function ajaxResultParses(inData) {
+  for (var i = 0; i < dataList.length; i++) {
 
-  var ress = inData.results;
-  for (var i = 0; i < ress.length; i++) {
+    var data = dataList[i];
+    var tempData = {
 
-    var res = ress[i];
-    var title = res.title;
-    var title_orig = res.original_title;
-    var language = res.original_language;
-    var vote = res.vote_average;
+      title: data.title,
+      title_orig: data.original_title,
+      language: data.original_language,
+      vote: data.vote_average,
+    }
 
-    addTitle(title, title_orig, language, vote);
+    var film = compiled(tempData);
+    container.append(film);
   }
 }
 
-
-function ajaxTest() {
-
-  var lista = $("li");
-  var lines = $("hr");
-  lista.remove();
-  lines.remove();
+function ajaxSearchMovie(content) {
 
   var outData = {
 
     api_key: "edb8c608c84aa9d386ab6be738961e5f",
     language: "it-IT",
-    query: inputSearch(),
+    query: content,
   };
 
   $.ajax({
@@ -54,7 +51,13 @@ function ajaxTest() {
     data: outData,
     success: function(inData){
 
-      ajaxResultParses(inData);
+      var res = inData.results;
+      var count = res.length;
+
+      if (count > 0) {
+
+        printFilms(res);
+      }
 
     },
     error: function(request, state, error){
@@ -65,29 +68,103 @@ function ajaxTest() {
   });
 }
 
+
+
 function inputSearch() {
 
   var me = $("#input_search");
   var content = me.val().toLowerCase();
 
-  return content;
+  ajaxSearchMovie(content);
+}
+
+function keyAction(e) {
+
+  if (e.which == 13) {
+    inputSearch();
+  }
 }
 
 
 function init() {
 
-  var doc = $(document);
-
   var inputbtn = $("#btn_search");
-  inputbtn.on("click", ajaxTest);
+  inputbtn.click(inputSearch);
 
   var input = $("#input_search");
-  input.keyup(function(e) {
-    if (e.which == 13) {
-      ajaxTest();
-    };
-  });
+  input.keyup(keyAction)
 
+  // var inputbtn = $("#btn_search");
+  // inputbtn.on("click", inputSearch);
 }
 
 $(document).ready(init);
+
+
+
+// function addTitle(title, title_orig, language, vote) {
+//
+//   var tempData = {
+//
+//     title: title,
+//     title_orig: title_orig,
+//     language: language,
+//     vote: vote,
+//   }
+//
+//   var template = $("#film_template").html();
+//   var compiled = Handlebars.compile(template);
+//   var li = compiled(tempData);
+//
+//   var ulFilms = $(".films");
+//   ulFilms.append(li);
+// }
+//
+//
+// function ajaxResultParses(inData) {
+//
+//   var ress = inData.results;
+//   for (var i = 0; i < ress.length; i++) {
+//
+//     var res = ress[i];
+//     var title = res.title;
+//     var title_orig = res.original_title;
+//     var language = res.original_language;
+//     var vote = res.vote_average;
+//
+//     addTitle(title, title_orig, language, vote);
+//   }
+// }
+
+
+// function ajaxCall() {
+//
+//   var lista = $("li");
+//
+//   lista.remove();
+//
+//
+//   var outData = {
+//
+//     api_key: "edb8c608c84aa9d386ab6be738961e5f",
+//     language: "it-IT",
+//     query: inputSearch(),
+//   };
+//
+//   $.ajax({
+//
+//     url: "https://api.themoviedb.org/3/search/movie",
+//     method: "GET",
+//     data: outData,
+//     success: function(inData){
+//
+//       ajaxResultParses(inData);
+//
+//     },
+//     error: function(request, state, error){
+//       console.log("request", request);
+//       console.log("state", state);
+//       console.log("error", error);
+//     }
+//   });
+// }
